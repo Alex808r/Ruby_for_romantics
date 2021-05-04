a4898209090f324c2cdb7ce90dcdfe5a7f5c7c21
Alex808r <fromxlam@gmail.com>
2021-05-04 12:43:33 +0300

    change app 23

--- app23.rb
+++ app23.rb
@@ -21,3 +21,43 @@ arr.each do |item|
 end
 
 puts hh.inspect
+
+
+#Можно написать короче, если установить значение хеша по умолчанию
+str1 = 'the quick brown fox jumps over the lazy dog dog'
+arr1 = str1.split(' ')
+
+hh1 = Hash.new(0)
+arr1.each do |word|
+  hh1[word] += 1
+end
+puts hh1.inspect
+
+
+#Задание: напишите программу, которая считает частотность букв и выводит на экран
+# список букв и их количество в предложении.
+
+
+str2 = 'the quick brown fox jumps over the lazy dog dog'
+arr2 = str2.split('')
+
+hh2 ={}
+arr2.each do |letter|
+  if hh2[letter].nil?
+    hh2[letter] = 1
+  else
+    hh2[letter] += 1
+  end
+end
+
+puts hh2
+
+# короткая версия
+str3 = 'tthe quick brown fox jumps over the lazy dog dog'
+arr3 = str3.split('')
+hh3 = Hash.new(0)
+arr3.each do |letter|
+  hh3[letter] += 1
+end
+
+puts hh3
