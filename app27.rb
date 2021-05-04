0798c4644aa94827018d45b32eba309f157fc080
Alex808r <fromxlam@gmail.com>
2021-05-04 15:06:30 +0300

    add app 27

---
+++ app27.rb
@@ -0,0 +1,44 @@
+=begin
+Задание: напишите класс Monkey (“обезьянка”). В классе должно быть
+1) реализовано два метода: run, stop;
+2) каждый из методов должен менять состояние объекта;
+3) you must expose the state of an object так, чтобы можно было узнать о состоянии класса
+снаружи, но нельзя было его модифицировать (к сожалению, точно перевести на русский
+язык выражение “expose the state” не получилось. Посмотрите перевод слова “expose” в
+словаре). Создайте экземпляр класса Monkey, вызовите методы объекта и проверьте
+работоспособность программы.
+Задание: сделайте так, чтобы при инициализации класса Monkey экземпляру
+присваивалось случайное состояние. Создайте массив из десяти обезьянок. Выведите
+состояние всех элементов массива на экран.
+=end
+
+class Monkey
+  attr_reader :status
+  def initialize
+    default_status = [:sleep, :run, :stop, :jump]
+    @status = default_status.sample
+  end
+
+  def run
+    @status = :run
+  end
+
+  def stop
+    @status = :stop
+  end
+end
+
+m1 = Monkey.new
+puts m1.status
+puts m1.stop
+puts m1.run
+puts m1.status
+puts '=' * 80 
+#инициализируем массив
+arr = []
+#добавляем в массив 10 экземпляров класса Monkey
+1.upto(10) { arr << Monkey.new }
+
+#Выводим состояние
+arr.each {|item| print "#{item.status}, "}
+
