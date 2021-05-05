128bef110dbefb310ea6e81086825c2f6d4268e3
Alex808r <fromxlam@gmail.com>
2021-05-05 18:17:41 +0300

    add app 32

--- app32.rb
+++ app32.rb
@@ -1,1 +1,45 @@
+# попробуйте написать программу по диаграмме классов на представленном рисунке.
 
+class Player
+  attr_accessor :x, :y
+  def initialize(options={})
+    @x = options[:x] || 0
+    @y = options[:y] || 0
+  end
+  def right
+    self.x += 1
+  end
+
+  def left
+    self.x -= 1
+  end
+  def up
+    self.y += 1
+  end
+  def down
+    self.y -= 1
+  end
+end
+class Robot < Player
+
+  def label
+    '*'
+  end
+end
+
+class Dog < Player
+  def up
+  end
+  def left
+  end
+  def label
+    '@'
+  end
+end
+
+class Human < Player
+
+  def label
+    'H'
+  end
+end
